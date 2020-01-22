6f859f66a909f4cfbc6ff9476e5c309fb80ce48f
Joshua Kuruvilla <joshkuruvilla512@gmail.com>
2020-01-21 23:00:03 -0600

    I believe that the Employee.js is finished.

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,8 +1,39 @@
 const inquirer = require("inquirer");
-
-function () {
+//Propertiesssss
+class Employee {
+    constructor(name, id, email) {
     this.name = name;
-    this.email = email;
     this.id = id;
-    this.role = role;
-};
+    this.email = email;
+}
+//Methodsssss
+
+getName() {
+    return this.name
+}
+
+getId() {
+    return this.id
+}
+
+getEmail() {
+    return this.email
+}
+
+getName() {
+    return this.name
+}
+
+getRole() {
+    return "Employee"
+}
+}
+
+module.exports = Employee
+
+// function () {
+//     this.name = name;
+//     this.email = email;
+//     this.id = id;
+//     this.role = role;
+// };
