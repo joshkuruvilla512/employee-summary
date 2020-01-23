1f03bec422e7d02338fb57f262e4bf253afbdaf2
Joshua Kuruvilla <joshkuruvilla512@gmail.com>
2020-01-22 19:23:40 -0600

    I have added individual questions for Manager, Engineer and Intern.

--- Develop/app.js
+++ Develop/app.js
@@ -5,57 +5,66 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 
 // Inquirer Questions
-
-inquirer
-    .prompt([
-        //Start by prompting for user role
-        {
-            type: "input",
-            name: "name",
-            message: "What is your name?"
-        },
-        {
-            type: "input",
-            name: "employeeId",
-            message: "What is your employee ID?"
-        },
-        {
-            type: "list",
-            message: "What is your company role?",
-            name: "role",
-            choices: [
-                "engineer",
-                "intern",
-                "manager"
-            ]
-        },
-
-        ////////////// Employee Information
-    ]).then(
-        function (answer) {
-            roleQuestions(answer.role)
-        }
-    )
-
-function roleQuestions(role) {
-    console.log("role: ", role)
-    const questions = [];
-    if (role === "engineer") {
-        questions.push(
+function getEmployeeInfo() {
+    inquirer
+        .prompt([
+            //Start by prompting for user role
             {
                 type: "input",
-                name: "username",
-                message: "Please enter your GitHub Username"
+                name: "name",
+                message: "What is your name?"
+            },
+            {
+                type: "list",
+                message: "What is your company role?",
+                name: "role",
+                choices: [
+                    "Manager",
+                    "Engineer",
+                    "Intern"
+                ]
+            },
+            {
+                type: "input",
+                name: "employeeId",
+                message: "What is your employee ID?"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is your email?"
+            },
+            {
+                type: "input",
+                name: "officeNumber",
+                message: "What is your Office Number?",
+                when: function (answers) {
+                    return answers.role === 'Manager';
+                }
+            },
+            {
+                type: "input",
+                name: "gitHub",
+                message: "What is your Github name?",
+                when: function (answers) {
+                    return answers.role === 'Engineer';
+                }
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "What School did you attend?",
+                when: function (answers) {
+                    return answers.role === 'Intern';
+                }
             }
-        )
-    }
-    inquirer
-        .prompt(
+            ////////////// Employee Information
+        ]).then(
+            function (answers) {
 
-            ////////////// if engineer
-            questions
-           
-
-        )
+            }
+        );
 }
+
+getEmployeeInfo();
 
